dac16dbbfa590c38d7a1a0f4b88cda77269332ef
Bhagyalakshmi1685 <63142881+Bhagyalakshmi1685@users.noreply.github.com>
2020-10-20 10:30:13 +0530

    Update TestClass.java

--- src/test/java/snippet/TestClass.java
+++ src/test/java/snippet/TestClass.java
@@ -6,10 +6,9 @@ public class TestClass {
 	@Test
 	public void cmdTest()
 	{
-		String BROWSER = System.getProperty("browser");
-		String URL = System.getProperty("url");
-		System.out.println(BROWSER);
-		System.out.println(URL);
+		
+		System.out.println("welcome to jenkins");
+		
 		
 		
 	}
